22c8c35db2593e75025d9378fa38f3b7d2fb437a
Julio Nicolas <jnicolas.marcos@gmail.com>
2020-02-07 16:39:14 -0600

    init

---
+++ FantasticView/FantasticView.h
@@ -0,0 +1,19 @@
+//
+//  FantasticView.h
+//  FantasticView
+//
+//  Created by Julio Nicolas on 2/7/20.
+//  Copyright © 2020 jn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FantasticView.
+FOUNDATION_EXPORT double FantasticViewVersionNumber;
+
+//! Project version string for FantasticView.
+FOUNDATION_EXPORT const unsigned char FantasticViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FantasticView/PublicHeader.h>
+
+
